3083e6f866ff791f9a640cdb6018872cf278b206
Amardeep Singh <30592051+Amardeep-Singh-Developer@users.noreply.github.com>
2025-11-13 17:49:16 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,14 +4,10 @@ set -e
 # Go to project folder
 cd /home/ec2-user/server
 
-# Remove old dependencies and builds (optional)
-rm -rf node_modules
-rm -rf dist
+# ✅ Install dependencies
+npm install
 
-# Install required dependencies
-# npm install
-
-# Build the Angular app
+# ✅ Build the Angular app
 npm run build --prod
 
-echo "✅ Angular build completed successfully."
+echo "✅ AfterInstall: Angular build completed successfully."
