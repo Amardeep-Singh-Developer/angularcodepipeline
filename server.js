1a2f368767eced9fe10586fc67a9fbc4cbf823f6
Amardeep Singh <30592051+Amardeep-Singh-Developer@users.noreply.github.com>
2025-11-13 21:09:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/stolt/index.html"));
 });
 
-const PORT = process.env.PORT || 3009;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`✅ Angular app is running on port ${PORT}`);
 });
